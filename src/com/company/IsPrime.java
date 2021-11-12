00d2bf01b73b369a38eb6817ec6b92e464837fc9
coder-dipesh <dipeshsiwakoti112@gmail.com>
2021-11-12 18:51:12 +0545

    algorithm to check prime numbers

---
+++ src/com/company/IsPrime.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class IsPrime {
+    int i;
+    // First method to find prime number (n)
+    public boolean isPrime(int n){
+        for(i=2 ; i<n ; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Second method to find prime number (n/2)
+    public boolean isPrime2(int n){
+        for (i=2 ; i< n/2; i++){
+            if (n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Third method to calculate prime number (n*n)
+
+    public boolean isPrime3(int n){
+        for (i=2 ; i<n*n ; i++){
+            if (n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
