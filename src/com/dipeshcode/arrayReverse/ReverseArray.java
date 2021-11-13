eaee4366851404ba6ce205490e1bc48451123b75
coder-dipesh <dipeshsiwakoti112@gmail.com>
2021-11-13 20:28:59 +0545

    Clear commit added

--- src/com/dipeshcode/arrayReverse/ReverseArray.java
+++ src/com/dipeshcode/arrayReverse/ReverseArray.java
@@ -1,15 +1,17 @@
 package com.dipeshcode.arrayReverse;
 
 public class ReverseArray {
-    int coll[]= {1,2,3,4,5,6,7};
+    int coll[]= {1,2,3,4,5,6,7}; // predefining the array
 
     public void reverseArray(){
-        for(int i=0; i<coll.length; i++){
+        for(int i=0; i<coll.length; i++){ // printing normal array
             System.out.print(coll[i]);
         }
+
         System.out.println("");
         System.out.println("Reversed array:");
-        for (int i= coll.length-1; i>=0;i--){
+        // printing reversed array
+        for (int i= coll.length-1; i>=0;i--){ // we go in backward direction to reverse array
             System.out.print(coll[i]);
         }
     }
